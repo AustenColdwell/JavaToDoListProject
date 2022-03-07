82d46d8c74aaa35e49c809957633cacf3dbbb2eb
Austen Coldwell <austen.coldwell@gmail.com>
2022-03-07 12:52:00 +0000

    Added read by Id function to the service and controller. Also added an exception class to handle items not found

--- src/main/java/com/qa/service/ToDoService.java
+++ src/main/java/com/qa/service/ToDoService.java
@@ -1,10 +1,12 @@
 package com.qa.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qa.Exception.ItemNotFoundException;
 import com.qa.entity.ListItem;
 import com.qa.repo.ToDoRepo;
 
@@ -25,6 +27,16 @@ public class ToDoService {
 	
 	public List<ListItem> getAllListItems(){
 		return this.repo.findAll();
+	}
+	
+	public ListItem getById(int id) {
+		Optional<ListItem> listOptional = this.repo.findById(id);
+		
+		if(listOptional.isPresent()) {
+			return listOptional.get();
+		}else {
+			throw new ItemNotFoundException("Item not found");
+		}
 	}
 
 }
