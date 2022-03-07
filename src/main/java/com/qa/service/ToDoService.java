50b9fb56b420d4587814b44bce5ab8cd4f0304af
Austen Coldwell <austen.coldwell@gmail.com>
2022-03-07 11:29:03 +0000

    Updated entity and added create functionality to the service and rest controller

--- src/main/java/com/qa/service/ToDoService.java
+++ src/main/java/com/qa/service/ToDoService.java
@@ -1,5 +1,24 @@
 package com.qa.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.entity.ListItem;
+import com.qa.repo.ToDoRepo;
+
+@Service
 public class ToDoService {
+	
+	@Autowired
+	private ToDoRepo repo;
+	
+	@Autowired
+	public ToDoService(ToDoRepo repo) {
+		this.repo = repo;
+	}
+	
+	public ListItem createListItem(ListItem listItem) {
+		return this.repo.save(listItem);
+	}
 
 }
