57e04014b837e95eb354ffa74e7aa917054de1ea
Austen Coldwell <austen.coldwell@gmail.com>
2022-03-07 12:16:30 +0000

    Added read all functionality to the service and controller classes

--- src/main/java/com/qa/controller/ToDoListController.java
+++ src/main/java/com/qa/controller/ToDoListController.java
@@ -1,8 +1,11 @@
 package com.qa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +24,16 @@ public class ToDoListController {
 		this.service = service;
 	}
 	
+	//========== CREATE ==========
 	@PostMapping("/create")
 	public ResponseEntity<ListItem> createItem(@RequestBody ListItem listItem){
 		return new ResponseEntity<ListItem>(this.service.createListItem(listItem), HttpStatus.ACCEPTED);
+	}
+	
+	//========== READ ==========
+	@GetMapping("/getAll")
+	public ResponseEntity<List<ListItem>> getAllItems(){
+		return new ResponseEntity<List<ListItem>>(this.service.getAllListItems(), HttpStatus.OK);
 	}
 
 }
